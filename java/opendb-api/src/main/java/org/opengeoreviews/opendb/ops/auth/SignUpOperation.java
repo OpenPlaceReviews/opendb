6a1e6bde1fb934a63d84c5ce6f07a67042f11f4b
Victor Shcherb <victor.shcherb@gmail.com>
2019-01-29 16:01:22 +0100

    Add operations

--- java/opendb-api/src/main/java/org/opengeoreviews/opendb/ops/auth/SignUpOperation.java
+++ java/opendb-api/src/main/java/org/opengeoreviews/opendb/ops/auth/SignUpOperation.java
@@ -1,34 +1,26 @@
 package org.opengeoreviews.opendb.ops.auth;
 
-import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Signature;
-import java.security.spec.ECGenParameterSpec;
 import java.util.Arrays;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
-import org.bouncycastle.crypto.generators.HKDFBytesGenerator;
-import org.bouncycastle.crypto.generators.SCrypt;
-import org.bouncycastle.crypto.params.ECDomainParameters;
-import org.bouncycastle.crypto.prng.FixedSecureRandom;
-import org.bouncycastle.jce.spec.ECParameterSpec;
 import org.opengeoreviews.opendb.ops.IOpenDBOperation;
 import org.opengeoreviews.opendb.ops.OpDefinitionBean;
 import org.opengeoreviews.opendb.ops.OpenDBOperation;
 import org.opengeoreviews.opendb.ops.OperationsRegistry;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@OpenDBOperation(LoginOperation.OP_ID)
-public class LoginOperation implements IOpenDBOperation {
+@OpenDBOperation(SignUpOperation.OP_ID)
+public class SignUpOperation implements IOpenDBOperation {
 
-	protected static final Log LOGGER = LogFactory.getLog(LoginOperation.class);
+	protected static final Log LOGGER = LogFactory.getLog(SignUpOperation.class);
 	
-	public static final String OP_ID = "login";
+	public static final String OP_ID = "signup";
 	private OpDefinitionBean definition;
 
 	@Override
@@ -70,17 +62,16 @@ public class LoginOperation implements IOpenDBOperation {
 
         KeyPair keyPair = getKeyPair();
 
-        // "8HEk28FDqegMkrrp";
         byte[] data = "test".getBytes("UTF8");
 
-        Signature sig = Signature.getInstance("SHA1withECDSA");
+        Signature sig = Signature.getInstance("SHA1WithRSA");
         sig.initSign(keyPair.getPrivate());
         sig.update(data);
         byte[] signatureBytes = sig.sign();
         System.out.println("Signature:" + Arrays.toString(signatureBytes));
 
         
-        sig = Signature.getInstance("SHA1withECDSA");
+        sig = Signature.getInstance("SHA1WithRSA");
         sig.initVerify(keyPair.getPublic());
         sig.update(data);
         
@@ -88,21 +79,9 @@ public class LoginOperation implements IOpenDBOperation {
         System.out.println(sig.verify(signatureBytes));
     }
 
-    private static KeyPair getKeyPair() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
-//        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-//        ECKeyPairGenerator pGen = new ECKeyPairGenerator();
-//        pGen.generateKeyPair();
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
-        ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
-        
-        byte[] bytes = "Hello world  + saltqwerqwerqwerqwer1231232112312321312".
-        		getBytes();
-        System.out.println(System.currentTimeMillis());
-        // "pwd__scrypt_nick_17_8_1_256__ec_secp256k1"
-        byte[] scrypt = SCrypt.generate(bytes, "vshcherb".getBytes(), 1 << 17, 8, 1, 256);
-        System.out.println(System.currentTimeMillis());
-        kpg.initialize(ecSpec, new FixedSecureRandom(scrypt));
-//        kpg.initialize(1024, new SecureRandom("Hello world  + salt".getBytes()));
+    private static KeyPair getKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(1024, new SecureRandom("Hello world  + salt".getBytes()));
         System.out.println(Arrays.toString(kpg.genKeyPair().getPublic().getEncoded()));
         System.out.println(Arrays.toString(kpg.genKeyPair().getPrivate().getEncoded()));
 //        kpg.initialize(1024);
